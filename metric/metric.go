bc41eb08af4d8a677d8d8a5db17b850a2c1c19d4
soarpenguin <soarpenguin@gmail.com>
2016-07-28 16:02:51 +0800

    fix cpu/mem usage percent calculate.

--- metric/metric.go
+++ metric/metric.go
@@ -99,12 +99,15 @@ func (self *Metric) UpdateStats(cid string, pid int) (map[string]uint64, error) 
 	}
 
 	info["docker.cpu.user"] = stats.CPUStats.CPUUsage.UsageInUsermode
-	info["docker.cpu.system"] = stats.CPUStats.CPUUsage.UsageInKernelmode
+	//info["docker.cpu.system"] = stats.CPUStats.CPUUsage.UsageInKernelmode
+	info["docker.cpu.system"] = stats.CPUStats.SystemCPUUsage
 	info["docker.cpu.usage"] = stats.CPUStats.CPUUsage.TotalUsage
+	info["cpuNum"] = uint64(len(stats.CPUStats.CPUUsage.PercpuUsage))
 	//FIXME in container it will get all CPUStats
 	info["docker.mem.usage"] = stats.MemoryStats.Usage
 	info["docker.mem.max_usage"] = stats.MemoryStats.MaxUsage
 	info["docker.mem.rss"] = stats.MemoryStats.Stats.Rss
+	info["memLimit"] = stats.MemoryStats.Limit
 
 	//FIXME use docker api network data.
 	if err := self.getNetStats(info); err != nil {
@@ -132,12 +135,32 @@ func (self *Metric) CalcRate(info map[string]uint64, now time.Time) (rate map[st
 	second_t := delta.Seconds()
 	for k, d := range info {
 		switch {
+		case strings.HasPrefix(k, "docker.cpu.usage") && d >= self.Save[k]:
+			var (
+				cpuPercent = 0.0
+				// calculate the change for the cpu usage of the container in between readings
+				cpuDelta = float64(d - self.Save[k])
+				// calculate the change for the entire system between readings
+				keysystem   = "docker.cpu.system"
+				systemDelta = float64(info[keysystem] - self.Save[keysystem])
+			)
+
+			if systemDelta > 0.0 && cpuDelta > 0.0 {
+				cpuPercent = (cpuDelta / systemDelta) * float64(info["cpuNum"]) * 100.0
+			}
+			rate[k] = cpuPercent
 		case strings.HasPrefix(k, "docker.cpu.") && d >= self.Save[k]:
-			rate[fmt.Sprintf("%s", k)] = float64(d-self.Save[k]) / nano_t
-		case strings.HasPrefix(k, "docker.disk.") && d >= self.Save[k]:
-			rate[fmt.Sprintf("%s", k)] = float64(d-self.Save[k]) / nano_t
+			rate[k] = float64(d-self.Save[k]) / nano_t
+		case strings.HasPrefix(k, "disk.") && d >= self.Save[k]:
+			rate[fmt.Sprintf("docker.%s", k)] = float64(d-self.Save[k]) / nano_t
 		case (strings.HasPrefix(k, gset.vlanPrefix) || strings.HasPrefix(k, gset.defaultVlan)) && d >= self.Save[k]:
 			rate[fmt.Sprintf("docker.%s", k)] = float64(d-self.Save[k]) * 8.0 / second_t
+		case strings.HasPrefix(k, "docker.mem.usage"):
+			var memPercent = 0.0
+			if info["memLimit"] != 0 {
+				memPercent = float64(d) / float64(info["memLimit"]) * 100.0
+			}
+			rate[k] = float64(memPercent)
 		case strings.HasPrefix(k, "docker.mem."):
 			rate[k] = float64(d)
 		}
